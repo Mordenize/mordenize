383dac459caec2b0e5de2c74111a57e32e09d035
duyhuynh <duyhuynh55110@gmail.com>
2025-01-29 13:41:15 +0700

    feat: run breeze install

--- app/database/migrations/0001_01_01_000000_create_users_table.php
+++ app/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,8 +17,12 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('role');
             $table->rememberToken();
+            $table->integer('created_by');
             $table->timestamps();
+            $table->integer('updated_by');
+            $table->softDeletes();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
